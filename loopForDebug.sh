237f2fc8e6f871c28d8198c49511468f667d6960
rarudo <rarudonet@gmail.com>
2017-06-14 10:14:43 +0900

    add loop.sh

---
+++ loopForDebug.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+array=("America" "Japan" "Australia" "China" "Russia" "Africa" "Europe" "Brazil")
+
+for((j=0;j<8;j++));do
+	for((i = 0; $i < 20; i++));do
+		ipLast=`printf "%02d" ${i}`
+		random=$((RANDOM % ${#array[@]}))
+		docker run -d --net=pi_network$j --ip=192.168.$j.1${ipLast} me/picurl curl_post 192.168.99.4:5000 ${array[${random}]}
+	done
+done
