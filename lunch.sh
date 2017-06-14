7eeb55851b5fd4c203cefa5aa39c326bba521372
rarudo <rarudonet@gmail.com>
2017-06-14 10:16:19 +0900

    fix lunch.sh

--- lunch.sh
+++ lunch.sh
@@ -3,5 +3,5 @@
 for((i = 0; $i < 20; i++));do
 	ipLast=`printf "%02d" ${i}`
 	random=$((RANDOM % ${#array[@]}))
-	docker run -d --net=pi_network$j --ip=192.168.1.1${ipLast} me/picurl curl_post 192.168.99.4:5000 ${array[${random}]}
+	docker run -d --net=pi_network --ip=192.168.1.1${ipLast} me/picurl curl_post 192.168.99.4:5000 ${array[${random}]}
 done
