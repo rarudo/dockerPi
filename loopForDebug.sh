753719234d1ae8e3475b43afc0a0994ee604855b
rarudo <rarudonet@gmail.com>
2017-06-18 05:57:48 +0900

    add rm option

--- loopForDebug.sh
+++ loopForDebug.sh
@@ -6,6 +6,6 @@ for((j=0;j<8;j++));do
 	for((i = 0; $i < 20; i++));do
 		ipLast=`printf "%02d" ${i}`
 		random=$((RANDOM % ${#array[@]}))
-		docker run -d --net=pi_network$j --ip=192.168.$j.1${ipLast} me/picurl curl_post 192.168.99.4:5000 ${array[${random}]}
+		docker run -d --rm --net=pi_network$j --ip=192.168.$j.1${ipLast} me/picurl curl_post 192.168.98.28:5000 ${array[${random}]}
 	done
 done
--- lunch.sh
+++ lunch.sh
@@ -2,5 +2,5 @@
 
 for((i = 0; $i < 20; i++));do
 	ipLast=`printf "%02d" ${i}`
-	docker run -d --net=pi_network --ip=192.168.1.1${ipLast} me/picurl curl_post 192.168.99.4:5000 hoge
+	docker run -d --rm --net=pi_network --ip=192.168.1.1${ipLast} me/picurl curl_post 192.168.99.4:5000 hoge
 done
