a82e958bd51407df4061d63a76adf7544fad2fc0
rarudo <rarudonet@gmail.com>
2017-06-17 21:41:51 +0000

    Fix lunch.sh

--- lunch.sh
+++ lunch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+bash /home/pirate/dockerPi/removeAll.sh 
 for((i = 0; $i < 20; i++));do
 	ipLast=`printf "%02d" ${i}`
-	docker run -d --rm --net=pi_network --ip=192.168.1.1${ipLast} me/picurl curl_post 192.168.99.4:5000 hoge
+	docker run -d --rm --net=pi_network --ip=86.105.227.1${ipLast} me/picurl curl_post 192.168.98.28:5000 Europe
 done
